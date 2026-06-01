13be9683d498dfe93b2563553eaac97b8a94b8c2
Haohua Zhao <huafang2@126.com>
2026-06-01 19:24:23 +0800

    small apk

--- app/src/main/java/per/jau/chargelog/service/ChargeLoggingService.kt
+++ app/src/main/java/per/jau/chargelog/service/ChargeLoggingService.kt
@@ -223,15 +223,13 @@ class ChargeLoggingService : Service() {
     }
 
     private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val serviceChannel = NotificationChannel(
-                CHANNEL_ID,
-                "充电日志服务通道",
-                NotificationManager.IMPORTANCE_LOW
-            )
-            val manager = getSystemService(NotificationManager::class.java)
-            manager.createNotificationChannel(serviceChannel)
-        }
+        val serviceChannel = NotificationChannel(
+            CHANNEL_ID,
+            "充电日志服务通道",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(serviceChannel)
     }
 
     companion object {
